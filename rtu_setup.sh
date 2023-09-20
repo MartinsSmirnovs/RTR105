54776b1056349c2fa882762f659b9730424eb182
MartinsSmirnovs <martinnc@inbox.lv>
2023-09-20 09:01:28 +0300

    added setup script

---
+++ rtu_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+code --install-extension shardulm94.trailing-spaces
+code --install-extension nathanridley.autotrim
+
+git clone https://github.com/MartinsSmirnovs/RTR105.git
+
+code RTR105
