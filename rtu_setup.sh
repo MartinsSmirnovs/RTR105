0c622e04825ae903a725b8b84174e71741b26b33
MartinsSmirnovs <martinnc@inbox.lv>
2023-09-20 09:46:56 +0300

    Added clang-format setup

--- rtu_setup.sh
+++ rtu_setup.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+set -e
+
+USERNAME=MartinsSmirnovs
+DIRECTORY=RTR105
+
+while getopts u:d:h flag
+do
+    case "${flag}" in
+        u) USERNAME=${OPTARG};;
+        d) DIRECTORY=${OPTARG};;
+        h) echo -e "Usage:\n./rtu_setup.sh -u GIT_USERNAME -d GIT_DIRECTORY" && exit 0;;
+    esac
+done
+
+echo
+echo "Installing Visual Studio Code extensions"
 code --install-extension shardulm94.trailing-spaces
 code --install-extension nathanridley.autotrim
+code --install-extension xaver.clang-format
 
-git clone https://github.com/MartinsSmirnovs/RTR105.git
+echo
+echo "Cloning git repository"
+git clone https://github.com/$USERNAME/$DIRECTORY.git
 
-code RTR105
+echo
+echo "Opening Visual Studio Code"
+code $DIRECTORY
+
+echo
+echo "Done!"
