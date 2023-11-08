6045a958ce26eb4cb9bfa2de5b8e2d4a607033ee
MartinsSmirnovs <something@inbox.lv>
2023-11-08 08:36:25 +0200

    Changed gnuplot extension in rtu_setup.sh

--- rtu_setup.sh
+++ rtu_setup.sh
@@ -20,7 +20,7 @@ code --install-extension shardulm94.trailing-spaces
 code --install-extension nathanridley.autotrim
 code --install-extension xaver.clang-format
 code --install-extension ms-vscode.cpptools
-code --install-extension mammothb.gnuplot
+code --install-extension fizzybreezy.gnuplot
 
 echo
 echo "Cloning git repository"
