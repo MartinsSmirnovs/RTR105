68592de05ce9bb9c6700b94bd7e367a1a33906a1
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2023-12-21 21:37:17 +0200

    Beautified integral code

--- class_21/main.c
+++ class_21/main.c
@@ -9,64 +9,64 @@ float input( const char* prompt )
     return value;
 }
 
-float asinIntegralUsingSteps( float lowerRange, float upperRange, float precision )
+enum Method
+{
+    Method_Step = 0,
+    Method_Trapezoid,
+    Method_Simpsons
+};
+
+float asinIntegralUsingSteps( float lowerRange, float step, float stepCount )
 {
     float integralValue = 0;
-    float integralValuePrevious = 0;
-    int stepCount = 1;
 
-    do
+    for ( int i = 0; i < stepCount; i++ )
     {
-        stepCount *= 2;
+        const float a = lowerRange + i * step;
 
-        const float step = ( lowerRange - upperRange ) / stepCount;
-
-        integralValuePrevious = integralValue;
-
-        integralValue = 0;
-        for ( int i = 0; i < stepCount; i++ )
-        {
-            // Step displacement is a form of optimization
-            const float stepDisplacement = step * 0.5;
-
-            integralValue += step * asin( lowerRange + i * step + stepDisplacement );
-        }
+        // Step displacement is a form of optimization
+        integralValue += step * asin( a + step / 2 );
     }
-    while ( fabs( integralValue - integralValuePrevious ) > precision );
 
     return integralValue;
 }
 
-float asinIntegralUsingSimpsonsRule( float lowerRange, float upperRange, float precision )
+float asinIntegralUsingSimpsonsRule( float lowerRange, float step, float stepCount )
 {
     float integralValue = 0;
-    float integralValuePrevious = 0;
-    int stepCount = 1;
 
-    do
+    // We have to do one iteration less due to the algorithm requiring one value forward
+    for ( int i = 0; i < stepCount - 1; i++ )
     {
-        stepCount *= 2;
+        const float a = lowerRange + step * i;
+        const float b = lowerRange + step * ( i + 1 );
 
-        const float step = ( lowerRange - upperRange ) / stepCount;
-
-        integralValuePrevious = integralValue;
-
-        integralValue = 0;
-        // We have to do one iteration less due to the algorithm requiring one value forward
-        for ( int i = 0; i < stepCount - 1; i++ )
-        {
-            integralValue += asin( lowerRange + step * i ) + 4 * asin( lowerRange + step * i + step / 2 ) + asin( lowerRange + step * ( i + 1 ) );
-        }
-
-        integralValue *= step / 6;
+        integralValue += asin( a ) + 4 * asin( ( a + b ) / 2 ) + asin( b );
     }
-    while ( fabs( integralValue - integralValuePrevious ) > precision );
+
+    return integralValue * step / 6;
+}
+
+float asinIntegralUsingTrapezoids( float lowerRange, float step, float stepCount )
+{
+    float integralValue = 0;
+
+    // We have to do one iteration less due to the algorithm requiring one value forward
+    for ( int i = 0; i < stepCount - 1; i++ )
+    {
+        const float a = lowerRange + step * i;
+        const float b = lowerRange + step * ( i + 1 );
+
+        const float point = asin( a );
+        const float pointNext = asin( b );
+
+        integralValue += ( point + pointNext ) * step / 2;
+    }
 
     return integralValue;
 }
 
-
-float asinIntegralUsingTrapezoids( float lowerRange, float upperRange, float precision )
+float asinIntegral( float lowerRange, float upperRange, float precision, enum Method method )
 {
     float integralValue = 0;
     float integralValuePrevious = 0;
@@ -79,16 +79,28 @@ float asinIntegralUsingTrapezoids( float lowerRange, float upperRange, float pre
         const float step = ( lowerRange - upperRange ) / stepCount;
 
         integralValuePrevious = integralValue;
-
         integralValue = 0;
 
-        // We have to do one iteration less due to the algorithm requiring one value forward
-        for ( int i = 0; i < stepCount - 1; i++ )
+        switch ( method )
         {
-            const float point = asin( lowerRange + i * step );
-            const float pointNext = asin( lowerRange + ( i + 1 ) * step );
-
-            integralValue += ( point + pointNext ) * step / 2;
+            case Method_Step:
+            {
+                integralValue = asinIntegralUsingSteps( lowerRange, step, stepCount );
+            }
+            break;
+            case Method_Trapezoid:
+            {
+                integralValue = asinIntegralUsingTrapezoids( lowerRange, step, stepCount );
+            }
+            break;
+            case Method_Simpsons:
+            {
+                integralValue = asinIntegralUsingSimpsonsRule( lowerRange, step, stepCount );
+            }
+            break;
+            default:
+                return -1;
+                break;
         }
     }
     while ( fabs( integralValue - integralValuePrevious ) > precision );
@@ -102,13 +114,13 @@ int main()
     const float b = input( "Please input upper range value b in range[a:1]:\n" );
     const float precision = input( "Please input precision value:\n" );
 
-    const float integralUsingSteps = asinIntegralUsingSteps( a, b, precision );
-    const float integralUsingTrapezoids = asinIntegralUsingTrapezoids( a, b, precision );
-    const float integralUsingSimpsonsRule = asinIntegralUsingSimpsonsRule( a, b, precision );
+    const float integralUsingSteps = asinIntegral( a, b, precision, Method_Step );
+    const float integralUsingTrapezoids = asinIntegral( a, b, precision, Method_Trapezoid );
+    const float integralUsingSimpsonsRule = asinIntegral( a, b, precision, Method_Simpsons );
 
-    printf( "asin(x) integral value in range [%.2f:%.2f] using step function method: %.2f\n", a, b, integralUsingSteps );
-    printf( "asin(x) integral value in range [%.2f:%.2f] using trapezoidal rule: %.2f\n", a, b, integralUsingTrapezoids );
-    printf( "asin(x) integral value in range [%.2f:%.2f] using Simpson's rule: %.2f\n", a, b, integralUsingSimpsonsRule );
+    printf( "asin(x) integral value in range [ %.5f : %.5f ] using step function method: %.5f\n", a, b, integralUsingSteps );
+    printf( "asin(x) integral value in range [ %.5f : %.5f ] using trapezoidal rule: %.5f\n", a, b, integralUsingTrapezoids );
+    printf( "asin(x) integral value in range [ %.5f : %.5f ] using Simpson's rule: %.5f\n", a, b, integralUsingSimpsonsRule );
 
     return 0;
 }
